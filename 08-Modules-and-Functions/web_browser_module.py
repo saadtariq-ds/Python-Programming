3cb528be0d55f703a3c1edc9e6c92adc7d65c89e
Saad Tariq <tariqsaad1997@gmail.com>
2024-12-06 02:13:18 +0500

    Modules and Functions

---
+++ 08-Modules-and-Functions/web_browser_module.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# webbrowser.open("https://www.python.org/downloads/release/python-3119/")
+
+# Will not work if Chrome is not installed
+chrome = webbrowser.get(using='google-chrome')
+chrome.open('https://www.udemy.com/course/python-the-complete-python-developer-course')
